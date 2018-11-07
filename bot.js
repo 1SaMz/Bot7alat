b4d98d8ffbd90a93ce483a3c399a86607f06fdf3
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-07 20:42:46 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const partly = new Discord.Client();
+const adminprefix = "-";
+const developers = ['498063352426135572'];
+
+console.log("3thhhhhhhhhhh2b.");
+
+
+partly.on('message', message => {
+    var argresult = message.content.split( ).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    partly.user.setGame(argresult);
+      message.channel.send(تم تغيير البلاينق الى   ${argresult})
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  partly.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(تَم تغيير الواتشينق الى   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  partly.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(تَم تغيير الليسينينق الى   ${argresult})
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    partly.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
+      message.channel.send(تم تغييرك حالتك بالتويتش الى   ${argresult})
+  }
+  if (message.content.startsWith(adminprefix + 'sn')) {
+  partly.user.setUsername(argresult).then
+      message.channel.send(جاري تغيير الأسم لـ ..${argresult} )
+} else
+if (message.content.startsWith(adminprefix + 'sa')) {
+  partly.user.setAvatar(argresult);
+    message.channel.send(جاري تغيير الأفتار... : );
+}
+});
+
+client.login(process.env.BOT_TOKEN);//
+
+
+
