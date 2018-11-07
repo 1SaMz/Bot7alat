70f72b41f9a0286d0b1694d64210c89331067d9e
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-07 21:05:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,43 @@
 const Discord = require('discord.js');
 const partly = new Discord.Client();
 const adminprefix = "-";
-const developers = ['434445470946033665'];
+const developers = ['324249224969584642'];
 
-console.log("3thhhhhhhhhhh2b.");
+console.log("Paaaaaaaaaaaartly");
 
 
 partly.on('message', message => {
-    var argresult = message.content.split( ).slice(1).join(' ');
+    var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     partly.user.setGame(argresult);
-      message.channel.send(تم تغيير البلاينق الى   ${argresult})
+      message.channel.send(`**Playing Now . ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   partly.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(تَم تغيير الواتشينق الى   ${argresult})
+      message.channel.send(`**Watching . ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   partly.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(تَم تغيير الليسينينق الى   ${argresult})
+      message.channel.send(`**Listening to .${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
     partly.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(تم تغييرك حالتك بالتويتش الى   ${argresult})
+      message.channel.send(`Now Streaming . **${argresult}**`)
   }
   if (message.content.startsWith(adminprefix + 'sn')) {
   partly.user.setUsername(argresult).then
-      message.channel.send(جاري تغيير الأسم لـ ..${argresult} )
+      message.channel.send(`Chaning Name To ..**${argresult}** `)
 } else
 if (message.content.startsWith(adminprefix + 'sa')) {
   partly.user.setAvatar(argresult);
-    message.channel.send(جاري تغيير الأفتار... : );
+    message.channel.send(`Changing Avatar To..**${argresult}** `);
 }
 });
-
 
 client.login(process.env.BOT_TOKEN);//
 
